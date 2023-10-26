34fb3af2c0a3a7213a1f6af0745b5c5a18eaa7b0
Maria Garcia <mariav2601@gmail.com>
2023-10-26 14:51:49 -0400

    routes changes

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,14 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\RecipeController;
+use App\Http\Controllers\Api\TagController;
 
 Route::get ("categories",            [CategoryController::class, "index"]);
 Route::get ("categories/{category}", [CategoryController::class, "show"]);
+
+Route::get ("recipes",          [RecipeController::class, "index"]);
+Route::get ("recipes/{recipe}", [RecipeController::class, "show"]);
+
+Route::get ("tags",       [TagController::class, "index"]);
+Route::get ("tags/{tag}", [TagController::class, "show"]);
