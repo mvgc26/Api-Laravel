f814866a2e21d2f38e7d2f4be53cdb07bb4c90c6
Maria Garcia <mariav2601@gmail.com>
2023-10-27 10:00:51 -0400

    updated
    cambio de las declaraciones de los route y declaraciones en los controllers

--- routes/api.php
+++ routes/api.php
@@ -10,11 +10,7 @@ use App\Http\Controllers\Api\TagController;
 Route::get ("categories",            [CategoryController::class, "index"]);
 Route::get ("categories/{category}", [CategoryController::class, "show"]);
 
-Route::get ("recipes",          [RecipeController::class, "index"]);
-Route::post ("recipes",          [RecipeController::class, "store"]);
-Route::get ("recipes/{recipe}", [RecipeController::class, "show"]);
-Route::put ("recipes/{recipe}", [RecipeController::class, "update"]);
-Route::delete ("recipes/{recipe}", [RecipeController::class, "destroy"]);
+Route::apiResource ("recipes", RecipeController::class);
 
 Route::get ("tags",       [TagController::class, "index"]);
 Route::get ("tags/{tag}", [TagController::class, "show"]);
