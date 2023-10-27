b8a5f045c8d116084e7073961ea482d081acf1ce
Maria Garcia <mariav2601@gmail.com>
2023-10-27 19:07:56 -0400

    update
    creacion

--- app/Http/Controllers/Api/TagController.php
+++ app/Http/Controllers/Api/TagController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Http\Resources\TagResource;
+
 use App\Models\Tag;
 
 class TagController extends Controller
